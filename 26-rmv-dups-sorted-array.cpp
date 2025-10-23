8b3ac988dd7ae4904d58b5369fb5ef32c06d92e9
huang-brian <60305099+huang-brian@users.noreply.github.com>
2025-10-22 18:30:08 -0700

    fixed 26 rmv dups from sorted array

--- 26-rmv-dups-sorted-array.cpp
+++ 26-rmv-dups-sorted-array.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.size() == 1)
+        int myElement = nums[0];
+        int my_non_dup_list_it = 1;
+        for (int i = 1; i < nums.size(); ++i)
         {
-            return 1;
-        }
-        int i = 0, non_dup_index = 1, unique_elements = 0;
-        for (; i < nums.size(); ++i)
-        {
-            while (non_dup_index<nums.size() && nums[i] == nums[non_dup_index])
+            if (nums[i] != myElement)
             {
-                non_dup_index++;
+                if (my_non_dup_list_it != i)
+                {
+                    nums[my_non_dup_list_it] = nums[i];
+                }
+                myElement = nums[i];
+                my_non_dup_list_it++;
             }
-            nums[i+1]=nums[non_dup_index];
-            non_dup_index++;
-            unique_elements++;
+            
         }
-
-        // for (int i = 0, non_dup_index = 1; i < nums.size() && non_dup_index < nums.size(); ++i)
-        // {
-        //     while (nums[i] == nums[non_dup_index] && non_dup_index < nums.size())
-        //     {
-        //         non_dup_index++;
-        //     }
-        //     nums[i] = nums[non_dup_index];
-        //     unique_elements++;
-        // }
-        return unique_elements;
+        return my_non_dup_list_it;
     }
 };
